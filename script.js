af1f97f3fe78e958724a1a89075dea9f5a4a76d4
centenocolqueg <centenocolqueg@gmail.com>
2026-06-12 18:09:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function mostrarSorpresa() {
+  const sorpresa = document.getElementById("sorpresa");
 
+  sorpresa.innerHTML = `
+    Solo quería recordarte algo:<br><br>
+    Me gusta verte feliz, me gusta cuando estás tranquila
+    y me gusta cuidarte bonito. ❤️<br><br>
+    Cuídate mucho, mi amorcito 🥺❤️
+  `;
+}
