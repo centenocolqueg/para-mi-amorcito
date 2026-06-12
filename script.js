5f07c485e9716a50dae5acbfe61d00baac7c7732
centenocolqueg <centenocolqueg@gmail.com>
2026-06-12 18:37:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ function mostrarSorpresa() {
   const sorpresa = document.getElementById("sorpresa");
 
   sorpresa.innerHTML = `
-    ✨ Solo quería recordarte algo bonito ✨<br><br>
-    Me gusta verte feliz, me gusta cuando estás tranquila
-    y me gusta cuidarte bonito. ❤️<br><br>
-    Eres muy especial para mí, mi amorcita 🥺🌹
+    ✨ Alexandra Cuti Hancco ✨<br><br>
+
+    Este detalle lo hice pensando solo en ti,
+    porque para mí no eres una persona más:
+    eres única, especial y muy bonita para mi vida. ❤️<br><br>
+
+    Me gusta verte sonreír, me gusta cuando estás tranquila
+    y me gusta cuando nos cuidamos con cariño. 🥺<br><br>
+
+    Tu nombre merece verse bonito aquí,
+    porque tú también tienes un lugar bonito en mi corazón. 🌹<br><br>
+
+    Eres mi amorcita especial, Alexandra. ❤️✨
   `;
 }
